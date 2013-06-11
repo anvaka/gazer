faf1d5834dfafffe0c92c8bc3bcc6cc5b9c51759
anvaka <anvaka@gmail.com>
2013-06-11 03:46:48 -0700

    Better projects list UI

--- app/scripts/controllers/costars.js
+++ app/scripts/controllers/costars.js
@@ -8,50 +8,71 @@ angular.module('githubStarsApp')
       $scope[logName] = msg;
     };
     var counter = new SortedOccurrenceCounter();
-
+    function UserFavoritesLogEntry(fields) {
+      angular.extend(this, fields);
+    }
     // if we know what to search, let's find it:
-    var projectName = $routeParams.q;
+    var analyzedProjectName = $routeParams.q;
     // todo: convert this to workflow.
-    if (projectName) {
+    if (analyzedProjectName) {
       var projectsOccurances = {};
       var updateHistogram = function (foundProjects) {
         for (var i = 0; i < foundProjects.length; ++i) {
           var projectName = foundProjects[i].full_name;
-          counter.add(projectName);
+          if (analyzedProjectName !== projectName) {
+            counter.add(projectName, foundProjects[i]);
+          }
         }
         $scope.projects = counter.list(100);
       };
       var processStarredProjects = function (followers) {
+        var totalUsers = followers.length;
         var processNextUser = function () {
           if (followers.length) {
             var userName = followers.pop().login;
             var usersProjectsCount = 0;
-            log('favoriteLog', "Processing projects starred by " + userName);
+            log('favoriteLog', new UserFavoritesLogEntry({
+                  userName: userName,
+                  processedCount: usersProjectsCount,
+                  step: totalUsers - followers.length,
+                  totalSteps: totalUsers
+                }));
+
             githubClient.getStarredProjects(userName).progress(function (progressReport){
               if (progressReport.total && progressReport.total > 30) {
                 // This guy has starred more than 3k projects. Let's ignore him.
                 // Tell github client to stop the process
-                log('droppedUsers', 'Skipping ' + userName + '; Reason: Starred ~' + progressReport.total * progressReport.perPage + ' projects');
+                log('droppedUsers', 'Skipping ' + userName + ': Starred ~' + progressReport.total * progressReport.perPage + ' projects');
                 return true;
               }
               usersProjectsCount += progressReport.data.length;
               updateHistogram(progressReport.data);
-              log('favoriteLog', "Processing projects starred by " + userName + ' (' + usersProjectsCount + ')');
+              log('favoriteLog', new UserFavoritesLogEntry({
+                  userName: userName,
+                  processedCount: usersProjectsCount,
+                  step: totalUsers - followers.length,
+                  totalSteps: totalUsers
+                }));
             }).then(function (data) {
               processNextUser();
+            }, function (err) {
+              debugger;
             });
+          } else {
+            log('favoriteLog', null);
+            log('done', "Analysis complete.");
           }
         };
         processNextUser();
       };
       var foundFollowers = [];
-      githubClient.getStargazers(projectName)
+      githubClient.getStargazers(analyzedProjectName)
         .progress(function (progressReport) {
           foundFollowers = foundFollowers.concat(progressReport.data);
-          log('followersLog', 'Gathering ' + projectName + ' followers: ' + foundFollowers.length);
+          log('followersLog', 'Gathering ' + analyzedProjectName + ' followers: ' + foundFollowers.length);
         })
         .then(function(res) {
-          log('followersLog', 'Found ' + foundFollowers.length + ' followers of ' + projectName);
+          log('followersLog', 'Found ' + foundFollowers.length + ' followers of ' + analyzedProjectName);
           processStarredProjects(foundFollowers);
         });
     }
