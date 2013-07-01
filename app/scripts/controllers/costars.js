5a22feff8bf866f4481e4732628a6e39752cc985
anvaka <anvaka@gmail.com>
2013-07-01 01:49:29 -0700

    Pulling pre-calculated recommendations when we can

--- app/scripts/controllers/costars.js
+++ app/scripts/controllers/costars.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('githubStarsApp')
-  .controller('CostarsCtrl', ['$scope', '$routeParams', '$cookies', 'promisingStream', 'githubClient', 'sortedOccurrenceCounter',
-              function ($scope, $routeParams, $cookies, promisingStream, githubClient, SortedOccurrenceCounter) {
+  .controller('CostarsCtrl', ['$scope', '$routeParams', '$cookies', 'promisingStream', 'precalculator', 'githubClient', 'sortedOccurrenceCounter',
+              function ($scope, $routeParams, $cookies, promisingStream, precalculator, githubClient, SortedOccurrenceCounter) {
     // TODO: this controller needs to be refactored - it violates SRP and contains more logic
     // than it should... I'm sorry about this mess, I will refactor this.
 
@@ -90,177 +90,196 @@ angular.module('githubStarsApp')
       return;
     }
     $scope.searchingLabel = 'Searching for "' + invariantProjectName + '"';
-
-    // Projects similarity calculation settings/tracking
-    var ourStargazersCount = 0; // number of stargazers who follow our project
-    var analyzedRatio = 1;      // % of our stargazers being analyzed
-    var counter = new SortedOccurrenceCounter(); // Our data model.
-    window.counter = counter;   // Developers are curious. Expose this for their exploration
-
-    // How we find a similarity between two projects?
-    var similarityCalculator = function (their, our, shared) {
-      // apprently this formula represents a generalization of Jaccard index 
-      // (http://en.wikipedia.org/wiki/Jaccard_index)
-      // Thanks to Cameron Davidson-Pilon for pointing out
-      return Math.round(100 * 2 * shared/(analyzedRatio * (their + our)));
-    };
-
-    // Sorting:
-    var sortTypes = {
-      '#' : 'sharedNumberOfStars',
-      '%' : 'sharedPercentOfStars'
-    };
-    $scope.sortBy = sortTypes['#'];
-    $scope.classForSort = function (sortType) {
-      if ((sortType === '#' && $scope.sortBy === 'sharedNumberOfStars') ||
-          (sortType === '%' && $scope.sortBy === 'sharedPercentOfStars')){
-        return 'current';
-      }
-      return '';
-    };
-    $scope.changeSort = function (sortType) {
-      $scope.sortBy = sortTypes[sortType];
-      updateSort();
-    };
-
-    var updateSort = function () {
-      if ($scope.sortBy === 'sharedPercentOfStars') {
-        var projects = [];
-        // TODO: percent sort is too slow and is not optimized yet.
-        var sorted = counter.customSort(ourStargazersCount, similarityCalculator);
-        for (var i = 0; i < settings.showResultRecords; ++i) {
-          projects.push(sorted[i]);
-        }
-        $scope.projects = projects;
-      } else {
-        // this is very fast. O(settings.showResultRecords).
-        $scope.projects = counter.list(settings.showResultRecords);
-      }
-    };
-
-    //Logging:
-    $scope.logEntries = {};
-    var log = function (logName, msg) {
-      $scope.logEntries[logName] = msg;
-    };
-
-    $scope.userStatus = {};
-    var updateUserAnalysisProgress = function (userName, processedCount, totalRecords) {
-      var record = $scope.userStatus[userName] || { name: userName};
-      record.processedCount = totalRecords ? (processedCount + '/' + totalRecords) : processedCount;
-      $scope.userStatus[userName] = record;
-    };
-    var removeUserAnalysisLogRecord = function (userName) {
-      delete $scope.userStatus[userName];
-    };
-    var updateOverallProgress = function (totalRecords, recordsAnalyzed) {
-      log('followersLog', 'Analyzed ' + recordsAnalyzed + ' out of ' + totalRecords + ' followers' );
-      ourStargazersCount = recordsAnalyzed;
-    };
-
-    var updateHistogram = function (foundProjects) {
-      for (var i = 0; i < foundProjects.length; ++i) {
-        var projectName = foundProjects[i].full_name;
-        if (invariantProjectName !== projectName.toLowerCase()) {
-          var projectData = foundProjects[i];
-          counter.add(projectName, {
-            watchers_count : projectData.watchers_count,
-            forks_count: projectData.forks_count,
-            description: projectData.description
-          });
-        }
-      }
-      updateSort();
-    };
-
-    var processStarredProjects = function (followers) {
-      var totalRecords = followers.length;
-      var usersAnalyzed = 0;
-      var createProgressUpdateHandler = function (userName) {
-        var processedCount = 0;
-        return function (progressReport) {
-          if (progressReport.totalPages && progressReport.totalPages > settings.maxStarredPagesPerUser) {
-            // This guy has starred too many projects. Let's ignore him.
-            // Tell github client to stop download remaining pages:
-            return true; // TODO: this is cryptic. Consider rejecting the promise?
-          }
-          processedCount += progressReport.data.length;
-          updateUserAnalysisProgress(userName,
-                                     processedCount,
-                                     progressReport.totalPages * progressReport.perPage);
-          updateHistogram(progressReport.data);
-        };
-      };
-
-      // we don't really need the entire hash of repo details to construct
-      // usage table:
-      var requiredRepoProperties = {
-        full_name: true,
-        watchers_count: true,
-        forks_count: true,
-        description: true
-      };
-
-      // Promising stream takes a burden of keeping at most N non resolved promises
-      // in parallel. As soon as a promise is resolved, the stream uses factroy method
-      // to create and run a new promise. It keeps going until all items in the array
-      // are converted to promises and resolved.
-      var discoveryProcess = promisingStream.process(followers, function (follower) {
-        var onGetStarredProjectProgressChanged = createProgressUpdateHandler(follower.login);
-
-        return githubClient.getStarredProjects(follower.login, requiredRepoProperties)
-                           .progress(onGetStarredProjectProgressChanged)
-                           .then(function(data){
-                              // so, we have data on this user. Remove him from the log:
-                              removeUserAnalysisLogRecord(follower.login);
-                              updateOverallProgress(totalRecords, ++usersAnalyzed);
-                              return data.length;
-                            }, function(reason){
-                              // todo: notify on UI?
-                              removeUserAnalysisLogRecord(follower.login);
-                              updateOverallProgress(totalRecords, ++usersAnalyzed);
-                              console.log(reason);
-                            });
-      }, settings.parallelUsersProcessing);
-
-      discoveryProcess.promise.then(function(result){
-        console.dir(result);
-        log('done', 'Done!');
-      });
-      return discoveryProcess;
-    };
-
-    var discoveryProcess, scopeDestroyed;
-    var foundFollowersCount = 0;
-
-    githubClient.getStargazers(invariantProjectName, {
-      login : true // we need only their login name here...
-    }).progress(function (progressReport) {
-      $scope.searchingLabel = '';
-      foundFollowersCount += progressReport.data.length;
-      log('followersLog', 'Gathering ' + invariantProjectName + ' followers: ' + foundFollowersCount);
-    }).then(function(foundFollowers) {
+    // this is getting super bad. I'm so excited to release this quicker, 
+    // but my techincal debt is enourmous here. Please excuse me!
+    function showPrecalculatedResults(similarProjects) {
       $scope.searchingLabel = '';
       $scope.repoTitle = invariantProjectName;
-      log('followersLog', 'Found ' + foundFollowers.length + ' followers of ' + invariantProjectName);
-      if (!scopeDestroyed) {
-        if (settings.starsCapEnabled && foundFollowers.length > settings.starsCap) {
-          analyzedRatio = settings.starsCap/foundFollowers.length;
-          // The array needs to be random, to avoid "mass stargazing"
-          // effect when project is featured on a news site:
-          foundFollowers = shuffleArray(foundFollowers);
-          foundFollowers.length = settings.starsCap;
-        }
-        discoveryProcess = processStarredProjects(foundFollowers);
-      }
-    }, function () {
-      $scope.searchingLabel = 'Sorry, I couldn\'t find this reposiotry on GitHub.com. Make sure it exists.';
-    });
+      $scope.displayOfflineResults = true;
+      $scope.projects = similarProjects;
+    }
 
-    $scope.$on('$destroy', function () {
-      if (discoveryProcess) {
-        discoveryProcess.cancel();
-        scopeDestroyed = true;
-      }
-    });
+    function makeOnlineCalculation(invariantProjectName) {
+      // Projects similarity calculation settings/tracking
+      var ourStargazersCount = 0; // number of stargazers who follow our project
+      var analyzedRatio = 1;      // % of our stargazers being analyzed
+      var counter = new SortedOccurrenceCounter(); // Our data model.
+      window.counter = counter;   // Developers are curious. Expose this for their exploration
+
+      // How we find a similarity between two projects?
+      var similarityCalculator = function (their, our, shared) {
+        // apprently this formula represents a generalization of Jaccard index 
+        // (http://en.wikipedia.org/wiki/Jaccard_index)
+        // Thanks to Cameron Davidson-Pilon for pointing out
+        return Math.round(100 * 2 * shared/(analyzedRatio * (their + our)));
+      };
+
+      // Sorting:
+      var sortTypes = {
+        '#' : 'sharedNumberOfStars',
+        '%' : 'sharedPercentOfStars'
+      };
+      $scope.sortBy = sortTypes['#'];
+      $scope.classForSort = function (sortType) {
+        if ((sortType === '#' && $scope.sortBy === 'sharedNumberOfStars') ||
+            (sortType === '%' && $scope.sortBy === 'sharedPercentOfStars')){
+          return 'current';
+        }
+        return '';
+      };
+      $scope.changeSort = function (sortType) {
+        $scope.sortBy = sortTypes[sortType];
+        updateSort();
+      };
+
+      var updateSort = function () {
+        if ($scope.sortBy === 'sharedPercentOfStars') {
+          var projects = [];
+          // TODO: percent sort is too slow and is not optimized yet.
+          var sorted = counter.customSort(ourStargazersCount, similarityCalculator);
+          for (var i = 0; i < settings.showResultRecords; ++i) {
+            projects.push(sorted[i]);
+          }
+          $scope.projects = projects;
+        } else {
+          // this is very fast. O(settings.showResultRecords).
+          $scope.projects = counter.list(settings.showResultRecords);
+        }
+      };
+
+      //Logging:
+      $scope.logEntries = {};
+      var log = function (logName, msg) {
+        $scope.logEntries[logName] = msg;
+      };
+
+      $scope.userStatus = {};
+      var updateUserAnalysisProgress = function (userName, processedCount, totalRecords) {
+        var record = $scope.userStatus[userName] || { name: userName};
+        record.processedCount = totalRecords ? (processedCount + '/' + totalRecords) : processedCount;
+        $scope.userStatus[userName] = record;
+      };
+      var removeUserAnalysisLogRecord = function (userName) {
+        delete $scope.userStatus[userName];
+      };
+      var updateOverallProgress = function (totalRecords, recordsAnalyzed) {
+        log('followersLog', 'Analyzed ' + recordsAnalyzed + ' out of ' + totalRecords + ' followers' );
+        ourStargazersCount = recordsAnalyzed;
+      };
+
+      var updateHistogram = function (foundProjects) {
+        for (var i = 0; i < foundProjects.length; ++i) {
+          var projectName = foundProjects[i].full_name;
+          if (invariantProjectName !== projectName.toLowerCase()) {
+            var projectData = foundProjects[i];
+            counter.add(projectName, {
+              watchers_count : projectData.watchers_count,
+              forks_count: projectData.forks_count,
+              description: projectData.description
+            });
+          }
+        }
+        updateSort();
+      };
+
+      var processStarredProjects = function (followers) {
+        var totalRecords = followers.length;
+        var usersAnalyzed = 0;
+        var createProgressUpdateHandler = function (userName) {
+          var processedCount = 0;
+          return function (progressReport) {
+            if (progressReport.totalPages && progressReport.totalPages > settings.maxStarredPagesPerUser) {
+              // This guy has starred too many projects. Let's ignore him.
+              // Tell github client to stop download remaining pages:
+              return true; // TODO: this is cryptic. Consider rejecting the promise?
+            }
+            processedCount += progressReport.data.length;
+            updateUserAnalysisProgress(userName,
+                                       processedCount,
+                                       progressReport.totalPages * progressReport.perPage);
+            updateHistogram(progressReport.data);
+          };
+        };
+
+        // we don't really need the entire hash of repo details to construct
+        // usage table:
+        var requiredRepoProperties = {
+          full_name: true,
+          watchers_count: true,
+          forks_count: true,
+          description: true
+        };
+
+        // Promising stream takes a burden of keeping at most N non resolved promises
+        // in parallel. As soon as a promise is resolved, the stream uses factroy method
+        // to create and run a new promise. It keeps going until all items in the array
+        // are converted to promises and resolved.
+        var discoveryProcess = promisingStream.process(followers, function (follower) {
+          var onGetStarredProjectProgressChanged = createProgressUpdateHandler(follower.login);
+
+          return githubClient.getStarredProjects(follower.login, requiredRepoProperties)
+                             .progress(onGetStarredProjectProgressChanged)
+                             .then(function(data){
+                                // so, we have data on this user. Remove him from the log:
+                                removeUserAnalysisLogRecord(follower.login);
+                                updateOverallProgress(totalRecords, ++usersAnalyzed);
+                                return data.length;
+                              }, function(reason){
+                                // todo: notify on UI?
+                                removeUserAnalysisLogRecord(follower.login);
+                                updateOverallProgress(totalRecords, ++usersAnalyzed);
+                                console.log(reason);
+                              });
+        }, settings.parallelUsersProcessing);
+
+        discoveryProcess.promise.then(function(result){
+          console.dir(result);
+          log('done', 'Done!');
+        });
+        return discoveryProcess;
+      };
+
+      var discoveryProcess, scopeDestroyed;
+      var foundFollowersCount = 0;
+
+      githubClient.getStargazers(invariantProjectName, {
+        login : true // we need only their login name here...
+      }).progress(function (progressReport) {
+        $scope.displayOnlineCalculation = true;
+        $scope.searchingLabel = '';
+        foundFollowersCount += progressReport.data.length;
+        log('followersLog', 'Gathering ' + invariantProjectName + ' followers: ' + foundFollowersCount);
+      }).then(function(foundFollowers) {
+        $scope.displayOnlineCalculation = true;
+        $scope.searchingLabel = '';
+        $scope.repoTitle = invariantProjectName;
+        log('followersLog', 'Found ' + foundFollowers.length + ' followers of ' + invariantProjectName);
+        if (!scopeDestroyed) {
+          if (settings.starsCapEnabled && foundFollowers.length > settings.starsCap) {
+            analyzedRatio = settings.starsCap/foundFollowers.length;
+            // The array needs to be random, to avoid "mass stargazing"
+            // effect when project is featured on a news site:
+            foundFollowers = shuffleArray(foundFollowers);
+            foundFollowers.length = settings.starsCap;
+          }
+          discoveryProcess = processStarredProjects(foundFollowers);
+        }
+      }, function () {
+        $scope.searchingLabel = 'Sorry, I couldn\'t find this reposiotry on GitHub.com. Make sure it exists.';
+      });
+
+      $scope.$on('$destroy', function () {
+        if (discoveryProcess) {
+          discoveryProcess.cancel();
+          scopeDestroyed = true;
+        }
+      });
+    }
+
+    precalculator.getPrecalculatedRecommendation(invariantProjectName)
+      .then(function(projects){
+        showPrecalculatedResults(projects);
+      }, function () {
+        makeOnlineCalculation(invariantProjectName);
+      });
   }]);
---
+++ app/scripts/services/precalculator.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('githubStarsApp')
+  .factory('precalculator', function ($http) {
+    var endpoint = 'http://s3.amazonaws.com/github_yasiv/out/';
+    return {
+      getPrecalculatedRecommendation: function (projectName) {
+        return $http.get(endpoint + projectName + '.json')
+          .then(function (res) {
+            return res.data && res.data.map(function (r, idx) {
+              // unpack compressed records into object with meaningful names
+              return {
+                name : r.n,
+                description: r.d,
+                forks: r.f,
+                watchers: r.w,
+                rate: r.r,
+                place: idx + 1
+              };
+            });
+          });
+      }
+    };
+  });
